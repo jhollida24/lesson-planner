67094d998f1ed08c2cc4e76bcae7db2c89f25edd
Jeff Holliday <jholliday@squareup.com>
2026-02-08 11:42:09 -0800

    Add products section to Package.swift
    - Explicitly define executable product
    - Ensures lesson-planner binary is built correctly

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 let package = Package(
     name: "LessonPlanner",
     platforms: [.macOS(.v13)],
+    products: [
+        .executable(name: "lesson-planner", targets: ["lesson-planner"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
     ],
